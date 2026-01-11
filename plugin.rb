c7d2c663ee7dc91e5c20c5801fd8575bf7d4cf20
Panda_CC <geekrcloud@vip.qq.com>
2026-01-12 02:47:45 +0800

    修复

--- plugin.rb
+++ plugin.rb
@@ -19,6 +19,40 @@ end
 require_relative "lib/my_plugin_module/engine"
 
 after_initialize do
+  # 加载模型
+  %w[
+    coin_user_balance
+    coin_transaction
+    coin_invoice_request
+    coin_recharge_package
+    coin_discount_group
+    coin_discount_group_user
+    coin_payment_order
+  ].each do |model|
+    require_relative "app/models/my_plugin_module/#{model}"
+  end
+
+  # 加载服务
+  %w[
+    coin_service
+    invoice_service
+    discount_service
+    epay_service
+    payment_service
+  ].each do |service|
+    require_relative "lib/my_plugin_module/#{service}"
+  end
+
+  # 加载控制器
+  %w[
+    coin_controller
+    admin_controller
+    invoice_controller
+    pay_controller
+  ].each do |controller|
+    require_relative "app/controllers/my_plugin_module/#{controller}"
+  end
+
   Discourse::Application.routes.append do
     mount ::MyPluginModule::Engine, at: "/coin"
   end
